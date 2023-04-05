323268ddc54e47be576e6069e8c9c675854e296a
binaris00 <binaris000@gmail.com>
2023-04-05 19:09:25 -0300

    Added a command display all user info in a embed

--- user_info_things.py
+++ user_info_things.py
@@ -4,7 +4,7 @@ class user_info(discord.app_commands.Group):
 
     @discord.app_commands.command(name="name", description="display the user name")
     async def user_name(self, interation: discord.Interaction, user: discord.Member= None):
-        if user is None:
+        if user == None:
             data = interation.user.name
         else:
             data = user.name
@@ -12,7 +12,7 @@ class user_info(discord.app_commands.Group):
 
     @discord.app_commands.command(name="avatar", description="display the user avatar")
     async def user_avatar(self, interation: discord.Interaction, user: discord.Member= None):
-        if user is None:
+        if user == None:
             data = interation.user.avatar
         else:
             data = user.avatar
@@ -20,11 +20,25 @@ class user_info(discord.app_commands.Group):
 
     @discord.app_commands.command(name="id", description="display the user id")
     async def user_id(self, interation: discord.Interaction, user: discord.Member= None):
-        if user is None:
+        if user == None:
             data = interation.user.id
         else:
             data = user.id
         await interation.response.send_message(data)
-
+    @discord.app_commands.command(name="user_info", description="Display all the user info in a embed")
+    async def user_info_all(self, interation: discord.Interaction, user: discord.Member=None):
+        if user == None:
+            data = interation.user
+        else:
+            data = user
+        embed = discord.Embed(title=f"{data.name} Info")
+        embed.set_thumbnail(url=data.avatar)
+        embed.add_field(name="Name:", value=data.name)
+        embed.add_field(name="Nick", value=data.nick)
+        embed.add_field(name="id", value=data.id)
+        embed.add_field(name="Joined at", value=data.joined_at)
+        
+        await interation.response.send_message(embed=embed)
+        
 async def setup(bot):
    bot.tree.add_command(user_info(name="user", description="Display all user information"))
