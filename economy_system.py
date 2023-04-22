ed84708cd8da39dbab8a65972fefebfbfc0baa88
binaris00 <binaris000@gmail.com>
2023-04-22 14:26:02 -0300

    Added cooldown and tested all commands

--- economy_system.py
+++ economy_system.py
@@ -3,6 +3,7 @@ from discord import app_commands
 import random
 import discord
 
+
 class Economy(commands.Cog):
    def __init__(self, bot):
       self.bot = bot
@@ -14,15 +15,15 @@ class Economy(commands.Cog):
    async def economy_start(self, interaction: discord.Interaction):
       if self.started == False:
          self.wallet = 1000
+         self.started = True
          embed = discord.Embed(title=f"{interaction.user.name} Start in the economy!!!", 
                                description="You start with 1000 coins in your wallet! Now simple commands you can use to generate more monet",
                                color=0x5773ff)
          embed.add_field(name="/work", value="Generate legal money with cooldown: 5 minutes")
          embed.add_field(name="/crime", value="Participe or make a crime to generate money, may be you can lose money, cooldown: 10 minutes")
-         embed.add_field(name="/rob (user)", value="Rob 30% a 60% from any user wallet, may be you can lose money, cooldown: 1 hour")
          embed.add_field(name="/deposit", value="Deposit money from your wallet for your bank")
          embed.add_field(name="/withdraw", value="Withdraw your money from your bank")
-         embed.add_field(name="money", value="See your wallet and bank money")
+         embed.add_field(name="/money", value="See your wallet and bank money")
          embed.add_field(name="Your wallet:", value=self.wallet)
          await interaction.response.send_message(embed=embed)
       else:
@@ -34,14 +35,23 @@ class Economy(commands.Cog):
          embed = discord.Embed(title="Your money", color=0x12d339)
          embed.add_field(name="Wallet", value=self.wallet)
          embed.add_field(name="Bank", value=self.bank)
+         await interaction.response.send_message(embed=embed)
+      else:
+         await interaction.response.send_message("Use '/economy_start' first to use this command")
          
          
+         
+         
+         
+         
+   
    # Thank you ChatGPT for the messages ;-;
    @app_commands.command(name="work", description="You want to get money with the legal way")
+   @app_commands.checks.cooldown(1, 300)
    async def economy_work(self, interaction: discord.Interaction):
       if self.started:
          money_earned = random.randint(100, 300)
-         with open("work_messages", "r", encoding="utf8") as file:
+         with open("work messages.txt", "r", encoding="utf8") as file:
             file = file.read()
             file = file.splitlines()
             message = random.choice(file)
@@ -51,10 +61,18 @@ class Economy(commands.Cog):
       else:
          await interaction.response.send_message("You need to use '/economy_start' first to work", ephemeral=True)
 
+   @economy_work.error
+   async def on_economy_work_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+      if isinstance(error, app_commands.CommandOnCooldown):
+         await interaction.response.send_message(error)
+   
+   
+   
    @app_commands.command(name="crime", description="You choice the ilegal way to earn money, maybe you can lose money")
+   @app_commands.checks.cooldown(1, 600)
    async def economy_crime(self, interaction: discord.Interaction):
       if self.started:
-         if random.random <= 0.6:
+         if random.random() <= 0.6:
             money_earned = random.randint(500, 700)
             self.wallet += money_earned
             with open("crime good messages.txt", "r", encoding="utf8") as file:
@@ -75,7 +93,14 @@ class Economy(commands.Cog):
                await interaction.response.send_message(embed=embed)
       else:
          await interaction.response.send_message("You need to use '/economy_start' first to do a crime", ephemeral=True)
-         
+   
+   @economy_crime.error
+   async def on_economy_crime_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+      if isinstance(error, app_commands.CommandOnCooldown):
+         await interaction.response.send_message(error)
+   
+   
+   
    @app_commands.command(name="deposit", description="Deposit money from your wallet for your bank")
    async def ecoomy_deposit(self, interaction: discord.Interaction, money: int):
       if self.started:
@@ -86,7 +111,7 @@ class Economy(commands.Cog):
          else:
             await interaction.response.send_message("You don't have that money!!", ephemeral=True)
       else:
-         await interaction.response.send_message("You need first use '/start'")
+         await interaction.response.send_message("You need first use '/economy_start'")
    
    @app_commands.command(name="withdraw", description="Withdraw your money from your bank")
    async def economy_withdraw(self, interaction: discord.Interaction, money: int):
@@ -104,7 +129,7 @@ class Economy(commands.Cog):
    async def economy_coin(self, interaction: discord.Interaction):
       if self.started:
          if self.wallet != 0:
-            if random.random <= 0.5:
+            if random.random() <= 0.5:
                self.wallet = self.wallet*2
                embed = discord.Embed(title="Coin", description=f"You win!!! Now you have {self.wallet}", color=0x12d339)
                await interaction.response.send_message(embed=embed)
@@ -119,4 +144,7 @@ class Economy(commands.Cog):
          
       else:
          await interaction.response.send_message("You don't have money to use '/coin_game', use '/economy_start'", ephemeral=True)
-   
+
+
+async def setup(bot):
+      await bot.add_cog(Economy(bot))
