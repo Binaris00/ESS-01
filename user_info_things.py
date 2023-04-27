877ae70fca25aa37ec69da6edeaed120e87ed31b
binaris00 <binaris000@gmail.com>
2023-04-26 21:56:42 -0300

    Minor changes

--- user_info_things.py
+++ user_info_things.py
@@ -3,34 +3,34 @@ import discord
 class user_info(discord.app_commands.Group):
 
     @discord.app_commands.command(name="name", description="display the user name")
-    async def user_name(self, interation: discord.Interaction, user: discord.Member= None):
+    async def user_name(self, interaction: discord.Interaction, user: discord.Member= None):
         if user == None:
-            data = interation.user.name
+            data = interaction.user.name
         else:
             data = user.name
-        await interation.response.send_message(data)
+        await interaction.response.send_message(data)
 
     @discord.app_commands.command(name="avatar", description="display the user avatar")
-    async def user_avatar(self, interation: discord.Interaction, user: discord.Member= None):
+    async def user_avatar(self, interaction: discord.Interaction, user: discord.Member= None):
         if user == None:
-            data = interation.user
+            data = interaction.user
         else:
             data = user
         embed = discord.Embed(title=f"{data.name} Avatar")
         embed.set_image(url=data.avatar)
-        await interation.response.send_message(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
     @discord.app_commands.command(name="id", description="display the user id")
-    async def user_id(self, interation: discord.Interaction, user: discord.Member= None):
+    async def user_id(self, interaction: discord.Interaction, user: discord.Member= None):
         if user == None:
-            data = interation.user.id
+            data = interaction.user.id
         else:
             data = user.id
-        await interation.response.send_message(data)
+        await interaction.response.send_message(data)
     @discord.app_commands.command(name="user_info", description="Display all the user info in a embed")
-    async def user_info_all(self, interation: discord.Interaction, user: discord.Member=None):
+    async def user_info_all(self, interaction: discord.Interaction, user: discord.Member=None):
         if user == None:
-            data = interation.user
+            data = interaction.user
         else:
             data = user
         embed = discord.Embed(title=f"{data.name} Info")
@@ -40,7 +40,7 @@ class user_info(discord.app_commands.Group):
         embed.add_field(name="id", value=data.id)
         embed.add_field(name="Joined at", value=discord.utils.format_dt(data.joined_at))
         
-        await interation.response.send_message(embed=embed)
+        await interaction.response.send_message(embed=embed)
         
 async def setup(bot):
    bot.tree.add_command(user_info(name="user", description="Display all user information"))
